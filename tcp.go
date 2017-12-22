04570e6d1597336343a6ab941a1dbb152b7f4f06
LanceLi <463499510@qq.com>
2017-12-22 20:32:28 +0800

    Update tcp.go

--- tcp.go
+++ tcp.go
@@ -9,8 +9,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/yinghuocho/gosocks"
-	"github.com/yinghuocho/gotun2socks/internal/packet"
+	"github.com/nange/gosocks"
+	"github.com/nange/gotun2socks/internal/packet"
 )
 
 type tcpPacket struct {
